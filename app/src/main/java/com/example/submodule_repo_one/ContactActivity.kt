4f664dfcc6c81f892bcb6cd94629919dcc9868f6
Daniel Ogunleye <daniel.ogunleye@engie.com>
2024-04-09 00:41:44 +0100

    some ui stuff

---
+++ app/src/main/java/com/example/submodule_repo_one/ContactActivity.kt
@@ -0,0 +1,12 @@
+package com.example.submodule_repo_one
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ContactActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+    }
+}
